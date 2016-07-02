ef20cd57454528913eb350a7d52acfce2300ac89
Zhenyu Fu <zhenyufu@usc.edu>
2016-07-01 21:06:58 -0700

    a big update

---
+++ theme/js/script.js
@@ -0,0 +1,31 @@
+// Open and close the sidenav on medium and small screens
+function w3_open() {
+    document.getElementById("my-side-text").style.display = "block";
+    document.getElementById("my-overlay").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("my-side-text").style.display = "none";
+    document.getElementById("my-overlay").style.display = "none";
+}
+
+// Accordions
+function myAccordion(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-theme";
+    } else { 
+        x.className = x.className.replace("w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-theme", "");
+    }
+}
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-65677627-2', 'auto');
+  ga('send', 'pageview');
+
