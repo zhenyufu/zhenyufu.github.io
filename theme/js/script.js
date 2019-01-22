82a7a13cc46fca337a3949dfbaa27f793588b021
zhenyufu <fzysmile@gmail.com>
2019-01-21 19:12:41 -0800

    AUTO: makefile publish static site

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,1 +1,24 @@
-function w3_open(){document.getElementById("my-side-text").style.display="block";document.getElementById("my-overlay").style.display="block"}function w3_close(){document.getElementById("my-side-text").style.display="none";document.getElementById("my-overlay").style.display="none"}function myAccordion(b){var a=document.getElementById(b);if(a.className.indexOf("w3-show")==-1){a.className+=" w3-show";a.previousElementSibling.className+=" w3-theme"}else{a.className=a.className.replace("w3-show","");a.previousElementSibling.className=a.previousElementSibling.className.replace(" w3-theme","")}};
+// Open and close the sidenav on medium and small screens
+function w3_open() {
+    document.getElementById("my-side-text").style.display = "block";
+    document.getElementById("my-overlay").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("my-side-text").style.display = "none";
+    document.getElementById("my-overlay").style.display = "none";
+}
+
+
+// Accordions
+function myAccordion(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-theme";
+    } else { 
+        x.className = x.className.replace("w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-theme", "");
+    }
+}
+
--- theme/js/scroll.js
+++ theme/js/scroll.js
@@ -1,1 +1,29 @@
-var slideIndex=0;carousel();function myMove(){var b;var a=document.getElementsByClassName("my-slides");for(b=0;b<a.length;b++){a[b].style.display="none"}if(slideIndex==a.length){slideIndex=1}else{if(slideIndex==-1){slideIndex=a.length-1}}a[slideIndex].style.display="block"}function plusDivs(a){slideIndex+=a;myMove()}function carousel(){slideIndex++;myMove();setTimeout(carousel,5000)};
+
+
+var slideIndex = 0;
+carousel();
+
+
+function myMove(){
+    var i;
+    var x = document.getElementsByClassName("my-slides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    if (slideIndex == x.length) {slideIndex = 1;} 
+    else if (slideIndex == -1) {slideIndex = x.length - 1;} 
+    x[slideIndex].style.display = "block";
+}
+
+
+function plusDivs(n) {
+    slideIndex += n;
+    myMove();
+}
+
+
+function carousel() {
+    slideIndex++;
+    myMove();
+    setTimeout(carousel, 5000); // Change image every 5 seconds
+}
